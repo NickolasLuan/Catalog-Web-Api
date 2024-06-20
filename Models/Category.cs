6a51246e573e82fb5a780cb5bd3f401c37ddb690
Nickolas Luan <Nickolasluan15@gmail.com>
2024-06-19 21:38:58 -0300

    First Commit

---
+++ Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.ObjectModel;
+
+namespace APICatalogo.Models;
+
+public class Category
+{
+
+    public Category()
+    {
+        Products = new Collection<Product>();
+    }
+
+    public int CategoryId { get; set; }
+    public string? Name { get; set; }
+    public string? ImageUrl { get; set; }
+    public ICollection<Product>? Products { get; set;}
+}
+
